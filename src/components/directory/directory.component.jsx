d151e03abd84a18a6ce05ca115b9930913f7dcee
tatjanatanaskovich <tatjanatanaskovich@gmail.com>
2019-09-04 15:01:49 +0200

    Changed images on the home page

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,32 +11,32 @@ class Directory extends React.Component {
       sections: [
         {
           title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          imageUrl: 'https://i.ibb.co/0Ywz577/sport-hat.jpg',
           id: 1,
           linkUrl: 'hats'
         },
         {
           title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+          imageUrl: 'https://i.ibb.co/SxFmfzt/jacket.jpg',
           id: 2,
           linkUrl: ''
         },
         {
           title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+          imageUrl: 'https://i.ibb.co/022wGgS/shoes.jpg',
           id: 3,
           linkUrl: ''
         },
         {
           title: 'womens',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+          imageUrl: 'https://i.ibb.co/gb2C5x9/w-model.jpg',
           size: 'large',
           id: 4,
           linkUrl: ''
         },
         {
           title: 'mens',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+          imageUrl: 'https://i.ibb.co/C1wSL6t/m-model.jpg',
           size: 'large',
           id: 5,
           linkUrl: ''
